f340a3aa078cf30ed9e32f50b185d5410d9f85b3
ravid7000 <ravi@technoarchsoftwares.com>
2017-06-07 13:12:11 +0530

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'Sign Up'})
+})
+
+module.exports = router;
---
+++ start.sh
@@ -0,0 +1,1 @@
+DEBUG=grey:* npm start
